50a835e650ff6406f5b7ad5d3d62e7e877f2cee0
polina <p.gurtovaya@gmail.com>
2015-12-14 15:28:37 -0800

    + Parser in index

--- javascripts/index.js
+++ javascripts/index.js
@@ -2,9 +2,10 @@ import Link from './components/link';
 import ReactRouterComponent from './components/routeMixin';
 import createDebuggerFor from './storages/routerModelDebugger'
 import * as utils from './helpers/utils'
+import Parser from './helpers/routeParser';
 
 import Router from './storages/routerModel';
 
-export {Link, ReactRouterComponent, createDebuggerFor, utils}
+export {Link, ReactRouterComponent, createDebuggerFor, utils, Parser}
 
 export default Router;
